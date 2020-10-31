931baebf146aab7fba754a5d10674d88af88acd6
Ivan Falcon <ivan.falcon@whiz.pe>
2020-10-31 10:03:04 -0500

    add routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountriesComponent } from './countries/countries.component';
+import { DishComponent } from './dish/dish.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'countries', component: CountriesComponent },
+  { path: 'dish', component: DishComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
